3feb2953ec60c0796d2e1c02483705b133bab464
Bartłomiej <b.mackiewicz88@wp.pl>
2023-03-30 16:07:27 +0200

    button_category

--- pages/my_home_search_button_page.py
+++ pages/my_home_search_button_page.py
@@ -8,12 +8,55 @@ class MyButtonSearchPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.long_menu_button = locators.ButtonLocators.long_menu_button
         self.js_button = locators.ButtonLocators.js_button
         self.is_js_visibility_assert = locators.ButtonLocators.is_js_visibility_assert
         self.html_button = locators.ButtonLocators.html_button
         self.is_html_visibility_assert = locators.ButtonLocators.is_html_visibility_assert
         self.php_button = locators.ButtonLocators.php_button
         self.is_php_visibility_assert = locators.ButtonLocators.is_php_visibility_assert
+        self.ruby_button = locators.ButtonLocators.ruby_button
+        self.is_ruby_visibility_assert = locators.ButtonLocators.is_ruby_visibility_assert
+        self.python_button = locators.ButtonLocators.python_button
+        self.is_python_visibility_assert = locators.ButtonLocators.is_python_visibility_assert
+        self.java_button = locators.ButtonLocators.java_button
+        self.is_java_visibility_assert = locators.ButtonLocators.is_java_visibility_assert
+        self.net_button = locators.ButtonLocators.net_button
+        self.is_net_visibility_assert = locators.ButtonLocators.is_net_visibility_assert
+        self.scala_button = locators.ButtonLocators.scala_button
+        self.is_scala_visibility_assert = locators.ButtonLocators.is_scala_visibility_assert
+        self.c_button = locators.ButtonLocators.c_button
+        self.is_c_visibility_assert = locators.ButtonLocators.is_c_visibility_assert
+        self.mobile_button = locators.ButtonLocators.mobile_button
+        self.is_mobile_visibility_assert = locators.ButtonLocators.is_mobile_visibility_assert
+        self.testing_button = locators.ButtonLocators.testing_button
+        self.is_testing_visibility_assert = locators.ButtonLocators.is_testing_visibility_assert
+        self.devops_button = locators.ButtonLocators.devops_button
+        self.is_devops_visibility_assert = locators.ButtonLocators.is_devops_visibility_assert
+        self.admin_button = locators.ButtonLocators.admin_button
+        self.is_admin_visibility_assert = locators.ButtonLocators.is_admin_visibility_assert
+        self.ux_ui_button = locators.ButtonLocators.ux_ui_button
+        self.is_ux_ui_visibility_assert = locators.ButtonLocators.ux_ui_button
+        self.pm_button = locators.ButtonLocators.pm_button
+        self.is_pm_visibility_assert = locators.ButtonLocators.is_pm_visibility_assert
+        self.game_button = locators.ButtonLocators.game_button
+        self.is_game_visibility_assert = locators.ButtonLocators.is_game_visibility_assert
+        self.analytics_button = locators.ButtonLocators.analytics_button
+        self.is_analytics_visibility_assert = locators.ButtonLocators.is_analytics_visibility_assert
+        self.security_button = locators.ButtonLocators.security_button
+        self.is_security_visibility_assert = locators.ButtonLocators.is_security_visibility_assert
+        self.data_button = locators.ButtonLocators.data_button
+        self.is_data_visibility_assert = locators.ButtonLocators.is_data_visibility_assert
+        self.go_button = locators.ButtonLocators.go_button
+        self.is_go_visibility_assert = locators.ButtonLocators.is_go_visibility_assert
+        self.support_button = locators.ButtonLocators.support_button
+        self.is_support_visibility_assert = locators.ButtonLocators.is_support_visibility_assert
+        self.erp_button = locators.ButtonLocators.erp_button
+        self.is_erp_visibility_assert = locators.ButtonLocators.is_erp_visibility_assert
+        self.architecture_button = locators.ButtonLocators.architecture_button
+        self.is_architecture_visibility_assert = locators.ButtonLocators.is_architecture_visibility_assert
+        self.other_button = locators.ButtonLocators.other_button
+        self.is_other_visibility_assert = locators.ButtonLocators.is_other_visibility_assert
 
     def open_page(self):
         self.driver.get("https://justjoin.it/")
@@ -24,8 +67,150 @@ class MyButtonSearchPage:
     def button_categories_html(self):
         self.driver.find_element(*self.html_button).click()
 
+    def button_categories_php(self):
+        self.driver.find_element(*self.php_button).click()
+
+    def button_categories_ruby(self):
+        self.driver.find_element(*self.ruby_button).click()
+
+    def button_categories_python(self):
+        self.driver.find_element(*self.python_button).click()
+
+    def button_categories_java(self):
+        self.driver.find_element(*self.java_button).click()
+
+    def button_categories_net(self):
+        self.driver.find_element(*self.net_button).click()
+
+    def button_categories_scala(self):
+        self.driver.find_element(*self.scala_button).click()
+
+    def button_categories_c(self):
+        self.driver.find_element(*self.c_button).click()
+
+    def button_categories_mobile(self):
+        self.driver.find_element(*self.mobile_button).click()
+
+    def button_categories_testing(self):
+        self.driver.find_element(*self.testing_button).click()
+
+    def button_categories_devops(self):
+        self.driver.find_element(*self.devops_button).click()
+
+    def button_categories_admin(self):
+        self.driver.find_element(*self.admin_button).click()
+
+    def button_categories_ux_ui(self):
+        self.driver.find_element(*self.ux_ui_button).click()
+
+    def button_categories_pm(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.pm_button).click()
+
+    def button_categories_game(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.game_button).click()
+
+    def button_categories_analytics(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.analytics_button).click()
+
+    def button_categories_security(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.security_button).click()
+
+    def button_categories_data(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.data_button).click()
+
+    def button_categories_go(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.go_button).click()
+
+    def button_categories_support(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.support_button).click()
+
+    def button_categories_erp(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.erp_button).click()
+
+    def button_categories_architecture(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.architecture_button).click()
+
+    def button_categories_other(self):
+        self.driver.find_element(*self.long_menu_button).click()
+        self.driver.find_element(*self.other_button).click()
+
     def is_js_visibility_displayed(self):
         return self.driver.find_element(*self.is_js_visibility_assert).is_displayed()
 
     def is_html_visibility_displayed(self):
         return self.driver.find_element(*self.is_html_visibility_assert).is_displayed()
+
+    def is_php_visibility_displayed(self):
+        return self.driver.find_element(*self.is_php_visibility_assert).is_displayed()
+
+    def is_ruby_visibility_displayed(self):
+        return self.driver.find_element(*self.is_ruby_visibility_assert).is_displayed()
+
+    def is_python_visibility_displayed(self):
+        return self.driver.find_element(*self.is_python_visibility_assert).is_displayed()
+
+    def is_java_visibility_displayed(self):
+        return self.driver.find_element(*self.is_java_visibility_assert).is_displayed()
+
+    def is_net_visibility_displayed(self):
+        return self.driver.find_element(*self.is_net_visibility_assert).is_displayed()
+
+    def is_scala_visibility_displayed(self):
+        return self.driver.find_element(*self.is_scala_visibility_assert).is_displayed()
+
+    def is_c_visibility_displayed(self):
+        return self.driver.find_element(*self.is_c_visibility_assert).is_displayed()
+
+    def is_mobile_visibility_displayed(self):
+        return self.driver.find_element(*self.is_mobile_visibility_assert).is_displayed()
+
+    def is_testing_visibility_displayed(self):
+        return self.driver.find_element(*self.is_testing_visibility_assert).is_displayed()
+
+    def is_devops_visibility_displayed(self):
+        return self.driver.find_element(*self.is_devops_visibility_assert).is_displayed()
+
+    def is_admin_visibility_displayed(self):
+        return self.driver.find_element(*self.is_admin_visibility_assert).is_displayed()
+
+    def is_ux_ui_visibility_displayed(self):
+        return self.driver.find_element(*self.is_ux_ui_visibility_assert).is_displayed()
+
+    def is_pm_visibility_displayed(self):
+        return self.driver.find_element(*self.is_pm_visibility_assert).is_displayed()
+
+    def is_game_visibility_displayed(self):
+        return self.driver.find_element(*self.is_game_visibility_assert).is_displayed()
+
+    def is_analytics_visibility_displayed(self):
+        return self.driver.find_element(*self.is_analytics_visibility_assert).is_displayed()
+
+    def is_security_visibility_displayed(self):
+        return self.driver.find_element(*self.is_security_visibility_assert).is_displayed()
+
+    def is_data_visibility_displayed(self):
+        return self.driver.find_element(*self.is_data_visibility_assert).is_displayed()
+
+    def is_go_visibility_displayed(self):
+        return self.driver.find_element(*self.is_go_visibility_assert).is_displayed()
+
+    def is_support_visibility_displayed(self):
+        return self.driver.find_element(*self.is_support_visibility_assert).is_displayed()
+
+    def is_erp_visibility_displayed(self):
+        return self.driver.find_element(*self.is_erp_visibility_assert).is_displayed()
+
+    def is_architecture_visibility_displayed(self):
+        return self.driver.find_element(*self.is_architecture_visibility_assert).is_displayed()
+
+    def is_other_visibility_displayed(self):
+        return self.driver.find_element(*self.is_other_visibility_assert).is_displayed()
--- tests/home_search_by_buttons.py
+++ tests/home_search_by_buttons.py
@@ -26,3 +26,157 @@ class TestButtonSearch:
         my_button_search_page.button_categories_html()
         assert my_button_search_page.is_html_visibility_displayed()
         time.sleep(5)
+
+    def test_button_search_php_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_php()
+        assert my_button_search_page.is_php_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_ruby_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_ruby()
+        assert my_button_search_page.is_ruby_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_python_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_python()
+        assert my_button_search_page.is_python_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_java_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_java()
+        assert my_button_search_page.is_java_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_net_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_net()
+        assert my_button_search_page.is_net_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_scala_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_scala()
+        assert my_button_search_page.is_scala_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_c_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_c()
+        assert my_button_search_page.is_c_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_mobile_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_mobile()
+        assert my_button_search_page.is_mobile_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_testing_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_testing()
+        assert my_button_search_page.is_testing_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_devops_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_devops()
+        assert my_button_search_page.is_devops_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_admin_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_admin()
+        assert my_button_search_page.is_admin_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_ux_ui_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_ux_ui()
+        assert my_button_search_page.is_ux_ui_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_pm_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_pm()
+        assert my_button_search_page.is_pm_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_game_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_game()
+        assert my_button_search_page.is_game_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_analytics_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_analytics()
+        assert my_button_search_page.is_analytics_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_security_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_security()
+        assert my_button_search_page.is_security_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_data_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_data()
+        assert my_button_search_page.is_data_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_go_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_go()
+        assert my_button_search_page.is_go_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_support_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_support()
+        assert my_button_search_page.is_support_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_erp_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_erp()
+        assert my_button_search_page.is_erp_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_architecture_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_architecture()
+        assert my_button_search_page.is_architecture_visibility_displayed()
+        time.sleep(5)
+
+    def test_button_search_other_passed(self):
+        my_button_search_page = MyButtonSearchPage(self.driver)
+        my_button_search_page.open_page()
+        my_button_search_page.button_categories_other()
+        assert my_button_search_page.is_other_visibility_displayed()
+        time.sleep(5)
