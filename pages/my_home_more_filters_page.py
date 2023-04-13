c2350c7851c1803abec7163877b693a174992aa4
Bartłomiej <b.mackiewicz88@wp.pl>
2023-04-13 14:24:28 +0200

    another_commit

--- pages/my_home_more_filters_page.py
+++ pages/my_home_more_filters_page.py
@@ -19,6 +19,16 @@ class MyMoreFiltersButton:
         self.all_button = locators.MoreFiltersLocators.all_button
         self.another_click_locations_button = locators.MoreFiltersLocators.another_click_locations_button
         self.message_offers_assert = locators.CitySelectionLocators.message_offers_assert
+        self.junior_button = locators.MoreFiltersLocators.junior_button
+        self.message_search_junior_assert = locators.MoreFiltersLocators.message_search_junior_assert
+        self.clear_filters_button = locators.MoreFiltersLocators.clear_filters_button
+        self.mid_button = locators.MoreFiltersLocators.mid_button
+        self.message_search_mid_assert = locators.MoreFiltersLocators.message_search_mid_assert
+        self.senior_button = locators.MoreFiltersLocators.senior_button
+        self.message_search_senior_assert = locators.MoreFiltersLocators.message_search_senior_assert
+        self.all_seniority_button = locators.MoreFiltersLocators.all_seniority_button
+        self.show_only_friendly_offers_checkbox = locators.MoreFiltersLocators.show_only_friendly_offers_checkbox
+        self.message_offers_ukraine_assert = locators.MoreFiltersLocators.message_offers_ukraine_assert
 
     def open_page(self):
         self.driver.get("https://justjoin.it/")
@@ -43,6 +53,35 @@ class MyMoreFiltersButton:
         self.driver.find_element(*self.all_button).click()
         self.driver.find_element(*self.show_offers_button).click()
 
+    def button_clear_filters(self):
+        self.driver.find_element(*self.another_click_locations_button).click()
+        self.driver.find_element(*self.clear_filters_button).click()
+
+    def button_more_filters_seniority_junior(self):
+        self.driver.find_element(*self.more_filters_button).click()
+        self.driver.find_element(*self.junior_button).click()
+        self.driver.find_element(*self.show_offers_button).click()
+
+    def button_more_filters_seniority_mid(self):
+        self.driver.find_element(*self.more_filters_button).click()
+        self.driver.find_element(*self.mid_button).click()
+        self.driver.find_element(*self.show_offers_button).click()
+
+    def button_more_filters_seniority_senior(self):
+        self.driver.find_element(*self.more_filters_button).click()
+        self.driver.find_element(*self.senior_button).click()
+        self.driver.find_element(*self.show_offers_button).click()
+
+    def button_more_filters_seniority_all(self):
+        self.driver.find_element(*self.another_click_locations_button).click()
+        self.driver.find_element(*self.all_seniority_button).click()
+        self.driver.find_element(*self.show_offers_button).click()
+
+    def button_more_filters_checkbox(self):
+        self.driver.find_element(*self.more_filters_button).click()
+        self.driver.find_element(*self.show_only_friendly_offers_checkbox).click()
+        self.driver.find_element(*self.show_offers_button).click()
+
     def is_message_search_b2b_displayed(self):
         return self.driver.find_element(*self.message_search_b2b_assert).is_displayed()
 
@@ -54,3 +93,15 @@ class MyMoreFiltersButton:
 
     def get_message_offers(self):
         return self.driver.find_element(*self.message_offers_assert).text
+
+    def is_message_search_junior_displayed(self):
+        return self.driver.find_element(*self.message_search_junior_assert).is_displayed()
+
+    def is_message_search_mid_displayed(self):
+        return self.driver.find_element(*self.message_search_mid_assert).is_displayed()
+
+    def is_message_search_senior_displayed(self):
+        return self.driver.find_element(*self.message_search_senior_assert).is_displayed()
+
+    def is_message_search_friendly_ukraine_displayed(self):
+        return self.driver.find_element(*self.message_offers_ukraine_assert).is_displayed()
--- tests/home_more_filters.py
+++ tests/home_more_filters.py
@@ -41,6 +41,65 @@ class TestMoreFiltersEmploymentTypeButton:
         assert msg in my_more_filters_page.get_message_offers()
         time.sleep(5)
 
+    def test_choice_of_form_of_employment_and_use_clear_filters_passed(self):
+        my_more_filters_page = MyMoreFiltersButton(self.driver)
+        my_more_filters_page.open_page()
+        my_more_filters_page.button_more_filters_employment_type_mandate_contract()
+        my_more_filters_page.button_clear_filters()
+        msg = "Offers with salary"
+        assert msg in my_more_filters_page.get_message_offers()
+        time.sleep(5)
 
-#@pytest.mark.usefixtures("setup")
-#class TestMoreFiltersSeniorityButton:
+
+@pytest.mark.usefixtures("setup")
+class TestMoreFiltersSeniorityButton:
+
+    def test_selection_of_experience_level_junior_passed(self):
+        my_more_filters_page = MyMoreFiltersButton(self.driver)
+        my_more_filters_page.open_page()
+        my_more_filters_page.button_more_filters_seniority_junior()
+        assert my_more_filters_page.is_message_search_junior_displayed()
+        time.sleep(5)
+
+    def test_selection_of_experience_level_mid_passed(self):
+        my_more_filters_page = MyMoreFiltersButton(self.driver)
+        my_more_filters_page.open_page()
+        my_more_filters_page.button_more_filters_seniority_mid()
+        assert my_more_filters_page.is_message_search_mid_displayed()
+        time.sleep(5)
+
+    def test_selection_of_experience_level_senior_passed(self):
+        my_more_filters_page = MyMoreFiltersButton(self.driver)
+        my_more_filters_page.open_page()
+        my_more_filters_page.button_more_filters_seniority_senior()
+        assert my_more_filters_page.is_message_search_senior_displayed()
+        time.sleep(5)
+
+    def test_selection_of_experience_level_all_passed(self):
+        my_more_filters_page = MyMoreFiltersButton(self.driver)
+        my_more_filters_page.open_page()
+        my_more_filters_page.button_more_filters_seniority_senior()
+        my_more_filters_page.button_more_filters_seniority_all()
+        msg = "Offers with salary"
+        assert msg in my_more_filters_page.get_message_offers()
+        time.sleep(5)
+
+    def test_selection_of_experience_and_use_clear_filters_passed(self):
+        my_more_filters_page = MyMoreFiltersButton(self.driver)
+        my_more_filters_page.open_page()
+        my_more_filters_page.button_more_filters_seniority_senior()
+        my_more_filters_page.button_clear_filters()
+        msg = "Offers with salary"
+        assert msg in my_more_filters_page.get_message_offers()
+        time.sleep(5)
+
+
+@pytest.mark.usefixtures("setup")
+class TestMoreFiltersCheckbox:
+
+    def test_more_filters_checkbox_passed(self):
+        my_more_filters_page = MyMoreFiltersButton(self.driver)
+        my_more_filters_page.open_page()
+        my_more_filters_page.button_more_filters_checkbox()
+        assert my_more_filters_page.is_message_search_friendly_ukraine_displayed()
+        time.sleep(5)
